87408959bfe83ee3e5f5d2d96e41c610f875dedd
Arif Babayev <101328505+ArifBabayev05@users.noreply.github.com>
2022-07-11 01:57:38 +0400

    Personal Portfolio

---
+++ index.js
@@ -0,0 +1,54 @@
+// import "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  var name = document.getElementById("name-field");
+  var company = document.getElementById("company-field");
+  var email = document.getElementById("email-field");
+  var message = document.getElementById("message-field");
+  var submit = document.getElementById("submit-btn");
+
+  function callEmailAPI(name, company, email, message) {
+    const options = {
+      method: "POST",
+      headers: {
+        "mode":"cors",
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "9cbb95a928msh2821046b9963db6p18054djsn98224f99e3f9",
+        "X-RapidAPI-Host": "rapidprod-sendgrid-v1.p.rapidapi.com",
+      },
+      body:
+        '{"personalizations":[{"to":[{"email":"Kingahmed19991@gmail.com"}],"subject":"Job Inquiry"}],"from":{"email":"' +
+        email +
+        '"},"content":[{"type":"text/plain","value":"' +
+        name +
+        " " +
+        company +
+        " " +
+        message +
+        '"}]}',
+    };
+
+    fetch("https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send", options)
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  }
+
+  submit.addEventListener("click", (e) => {
+    if
+    (name.value === "" || company.value === ""
+    || email.value === "" || message.value === "")
+    {
+      name.style.outline = "1px solid red"
+      company.style.outline = "1px solid red"
+      email.style.outline = "1px solid red"
+      message.style.outline = "1px solid red"
+      alert('Please fill out all fields!')
+    }
+    else{
+      callEmailAPI(name.value,company.value,email.value,message.value)
+      console.log(name.value,company.value,email.value,message.value);
+    }
+
+  });
+});
